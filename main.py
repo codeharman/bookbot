d63ddc56fcc48a5775962e89aafcb6a3fa2d23f4
Harmanjot Singh <161106030+codeharman@users.noreply.github.com>
2026-05-13 18:49:22 +0530

    read and open document added

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
-print('greetings boots')
+data = "./books/frankenstein.txt"
+
+# function goes here
+
+def get_book_text(obj):
+    with open(obj) as f:
+        return f.read()
+
+def main():
+    print(get_book_text(data))
+
+main()
 
 
