09e49cde0c08e6b4a775b34fb1e69f57c1d12d95
ThePrimeagen <161106030+codeharman@users.noreply.github.com>
2026-05-18 20:38:16 +0530

    refactor the code so that sys.argv is used so that we can use any book instead of the hard coded book

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from stats import get_num_words, converter, sorting
-
-data = "./books/frankenstein.txt"
+import sys
 
 # function goes here
+
+if len(sys.argv) != 2:
+    print('Usage: python3 main.py <path_to_book>')
+    sys.exit(1)
+
+book_path = sys.argv[1]
 
 def get_book_text(obj):
     with open(obj) as f:
@@ -14,9 +19,9 @@ def printer(items):
         result += f"{i['char']}: {i['num']}\n"
     return result
 
-get_num_words(get_book_text,data)
+get_num_words(get_book_text,book_path)
 
-result = converter(get_book_text, data)
+result = converter(get_book_text, book_path)
 
 sorting_result = sorting(result)
 
@@ -26,7 +31,7 @@ print(f"""
 ============ BOOKBOT ============
 Analyzing book found at books/frankenstein.txt...
 ----------- Word Count ----------
-{get_num_words(get_book_text,data)}
+{get_num_words(get_book_text,book_path)}
 --------- Character Count -------
 {printer_result}
 ============= END ===============
